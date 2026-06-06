270df16c00c27a64e5aaaeacea6ae032e5d374ab
Karl97490 <karl.pery@icloud.com>
2026-06-06 21:46:58 +0400

    setup axios

--- src/pages/BreadsList.jsx
+++ src/pages/BreadsList.jsx
@@ -1,9 +1,30 @@
 import "../styles/BreadsList.css"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 export const BreadsList = () => {
+    const [breads, setBreads] = useState([])
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const getData = async () => {
+        try {
+            const response = await axios.get()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div className="breads-page">
             <h1>This is BreadsList component...</h1>
+            {breads.map(bread => {
+                return (
+                    <h2>Title</h2>
+                )
+            })}
         </div>
     )
 }
--- src/pages/SandwichesList.jsx
+++ src/pages/SandwichesList.jsx
@@ -1,11 +1,30 @@
 import "../styles/SandwichesList.css"
-import { useState,useEffect } from "react"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 export const SandwichesList = () => {
+    const [sandwiches, setSandwiches] = useState([])
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const getData = async () => {
+        try {
+            const response = await axios.get()
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
         <div className="sandwiches-page">
             <h1>This is SandwichesList component...</h1>
+            {sandwiches.map(sandwich => {
+                return(
+                    <h2>Title</h2>
+                )
+            })}
         </div>
     )
 }
