60e31fbdfb5257cdb0e3e3db2a5ec9648b223a75
Karl97490 <karl.pery@icloud.com>
2026-06-12 12:04:40 +0400

    add TestUI component

---
+++ src/pages/TestUI.jsx
@@ -0,0 +1,19 @@
+// import { CardsLoading } from "../components/CardsLoading";
+// export const TestUI = () => {
+//   return (
+//     <div className="relative flex-1 w-full max-w-sm mx-0 md:mx-4">
+//       <div className="absolute -inset-4">
+//         <div
+//           className="w-full h-full mx-auto rotate-180 opacity-20 blur-lg filter"
+//           style={{
+//             background:
+//               "linear-gradient(90deg, #44ff9a -0.55%, #44b0ff 22.86%, #8b44ff 48.36%, #ff6644 73.33%, #ebff70 99.34%)",
+//           }}
+//         ></div>
+//       </div>
+//       <div className="relative overflow-hidden bg-white border-0 border-gray-200 rounded-2xl">
+//         <CardsLoading />
+//       </div>
+//     </div>
+//   );
+// };
